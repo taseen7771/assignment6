297a2ecc558c1937795a927bab9cdee211ae2cfd
taseen7771 <taseenmd98@gmail.com>
2025-06-07 21:59:19 +0530

    Update and rename README.md to output.js

---
+++ output.js
@@ -0,0 +1,41 @@
+# assignment6
+I'm a var variable
+basics.js:12 Taseen
+basics.js:13 3.14159
+basics.js:14 Hello, JavaScript!
+basics.js:15 42
+basics.js:16 true
+basics.js:17 Array(4)
+basics.js:18 Object
+basics.js:28 Addition: NaN
+basics.js:29 Subtraction: NaN
+basics.js:30 Multiplication: NaN
+basics.js:31 Division: NaN
+basics.js:48 Your grade is: F
+basics.js:51 1
+basics.js:51 2
+basics.js:51 3
+basics.js:51 4
+basics.js:51 5
+basics.js:51 6
+basics.js:51 7
+basics.js:51 8
+basics.js:51 9
+basics.js:51 10
+basics.js:51 11
+basics.js:51 12
+basics.js:51 13
+basics.js:51 14
+basics.js:51 15
+basics.js:51 16
+basics.js:51 17
+basics.js:51 18
+basics.js:51 19
+basics.js:51 20
+basics.js:56 2
+basics.js:56 4
+basics.js:56 6
+basics.js:56 8
+basics.js:56 10
+basics.js:61 Welcome, Taseen!
+basics.js:68 Square of 5 is: 25
